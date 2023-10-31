92eea07a3459d7a178ff665996d468d954e8c2c1
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2023-11-01 02:14:37 +0800

    add testing data oneinch

---
+++ test/visualizer/oneinch/data.ts
@@ -0,0 +1,18 @@
+import { OneInchLimitOrderV3 } from "../../../src/types/oneinch";
+
+const oneinchNormalLimitOrder: OneInchLimitOrderV3 = {
+  salt: "45118768841948961586167738353692277076075522015101619148498725069326976558864",
+  makerAsset: "0x5f04D47D698F79d76F85E835930170Ff4c4EBdB7",
+  takerAsset: "0x000075B45Dff84C00Cf597d5C3E766108CeA0000",
+  maker: "0xa88800cd213da5ae406ce248380802bd53b47647",
+  receiver: "0x11799622F4D98A24514011E8527B969f7488eF47",
+  allowedSender: "0xd9145CCE52D386f254917e481eB44e9943F39138",
+  takingAmount: "1000000000000000000",
+  makingAmount: "1420000000",
+  offsets: "0",
+  interactions: "0x000000000000000000000000000000090000000000000000000000000000008e",
+};
+
+Object.freeze(oneinchNormalLimitOrder);
+
+export { oneinchNormalLimitOrder };
