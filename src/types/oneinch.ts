ef0557eaa8757a0aacf45c964917ef0facd949cc
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2023-10-31 10:16:20 -0700

    Update type documentation

--- src/types/oneinch.ts
+++ src/types/oneinch.ts
@@ -5,28 +5,26 @@ import { BytesLike } from "ethers";
  * @see https://portal.1inch.dev/documentation/fusion/fusion-sdk/for-resolvers/auction-calculator
  */
 export type LimitOrderV3Struct = {
-  //a unique value necessary to be able to create limit orders with the same parameters and different hashes
+  // Contains of auction start time, auction duration, initial rate bump, fee and some unique value
   salt: string;
-  // token contract address of the asset user want to sell
+  // Address of the maker asset
   makerAsset: string;
-  // token contract address of the asset user want to buy
+  // Address of the taker asset
   takerAsset: string;
-  // the address of the limit order creator
+  // An address of the maker (wallet or contract address)
   maker: string;
-  /* If it contains a zero address, which means that taker asset will be sent to the address
-   * of the creator of the limit order. If user set any other value, then taker asset will
-   * be sent to the specified address */
+  /* An address of the wallet or contract who will receive filled amount. If it contains a zero address, 
+   * which means that taker asset will be sent to the address of the creator of the limit order. 
+     If user set any other value, then taker asset will be sent to the specified address */
   receiver: string;
-  /* If it contains a zero address, which means that a limit order is available for everyone to fill. 
-    If user set any other value, then the limit order will be available for execution only for the 
-    specified address (private limit order) */
+  // An address of the settlement contract
   allowedSender: string;
-  // amount of maker asset
+  // Order maker's token amount
   makingAmount: string;
-  // amount of taker asset
+  // Order taker's token amount
   takingAmount: string;
-  // every 32's bytes represents offset of the n'ths interaction
+  // Merged offsets of each field in interactions
   offsets: string;
-  // used to encode fusion specific data
+  // An interaction call data. ABI encoded set of makerAssetData, takerAssetData, getMakingAmount, getTakingAmount, predicate, permit, preInteraction, postInteraction
   interactions: BytesLike;
 };
