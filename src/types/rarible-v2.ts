ebf58fe3a67e127e3db5abc9297038c7f623b4e7
ryo hashimoto <ryo.hashimoto.2525@gmail.com>
2023-11-02 17:17:14 +0900

    comment: order fields

--- src/types/rarible-v2.ts
+++ src/types/rarible-v2.ts
@@ -3,22 +3,32 @@
  */
 
 export type RaribleV2AssetType = {
+  // asset class id, which is a hash value
   assetClass: string;
+  // this may include the token address and token id
   data: string;
 };
 
 export type RaribleV2Asset = {
   assetType: RaribleV2AssetType;
+  // amount of the asset
   value: string;
 };
 
 export type RaribleV2Order = {
+  // address of the maker
   maker: string;
+  // asset to be sold
   makeAsset: RaribleV2Asset;
+  // address of the taker
   taker: string;
+  // asset to be bought
   takeAsset: RaribleV2Asset;
+  // random value to identify order
   salt: number;
+  // asset auction's start time
   start: number;
+  // asset auction's end time
   end: number;
   dataType: string;
   data: string;
